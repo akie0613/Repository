8d414fdbd003df641fe1e7f5dbcbb20ab248038a
akie_naoki <akie_naoki@crossware.co.jp>
2020-08-19 13:22:24 +0900

    素数を求める_修正

--- src/Sosuu.java
+++ src/Sosuu.java
@@ -2,55 +2,30 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 public class Sosuu {
-    //2020/08/19 10:27~11:43
+    //2020/08/19 10:27~11:43　修正20分
     public static void main(String[] args) throws IOException {
         outputSosuu();
         outputSosuu2();
     }
+
     //入力された数字以下の素数を求めて全て表示させる
     public static void outputSosuu() throws IOException {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         System.out.println("数字を入力してください。(2-9999)");
-        String str;
+        int inputNum;
         while (true){
-            str  = br.readLine();
-            if(Integer.parseInt(str)<2||Integer.parseInt(str)>9999){
+            inputNum = Integer.parseInt(br.readLine());
+            if(inputNum<2||inputNum>9999){
                 System.out.println("2-9999の値を入力してください");
             }
             else {
                 break;
             }
         }
-        //最初に出力される素数の前にカンマをつけないためのフラグ作成
-        boolean isfirst = true;
-        int inputNum = Integer.parseInt(str);
-        //2が入力された場合のみ動きが違う
-        if(inputNum == 2){
-            System.out.println("2以下の素数は、2です。");
-        }
-        else{
-            System.out.print(inputNum + "以下の素数は、");
-            for(int i = 2; i < inputNum; i++){
-                boolean isSosuu = true;
-                for(int j = 2; j<i ; j++){
-                    if(i%j==0){
-                        isSosuu = false;
-                    }
-                }
-
-                if (isSosuu){
-                    if(isfirst){
-                        System.out.print(i);
-                        isfirst = false;
-                    }
-                    else {
-                        System.out.print(","+i);
-                    }
-                }
-            }
-            System.out.println("です。");
-        }
+        System.out.print(inputNum + "以下の素数は、");
+        outputSosuuList(2,inputNum);
+        System.out.println("です。");
     }
 
     //入力される数字を2つに増やす
@@ -59,38 +34,40 @@ public class Sosuu {
         InputStreamReader isr = new InputStreamReader(System.in);
         BufferedReader br = new BufferedReader(isr);
         System.out.println("数字を2つ入力してください。(2-9999)");
-        String str1;
-        String str2;
+        int inputNum1;
+        int inputNum2;
         while (true){
             System.out.println("1つめ：");
-            str1  = br.readLine();
-            if(Integer.parseInt(str1)<2||Integer.parseInt(str1)>9999){
+            inputNum1 = Integer.parseInt(br.readLine());
+            if(inputNum1<2||inputNum1>9999){
                 System.out.println("2-9999の値を入力してください");
                 continue;
             }
             System.out.println("2つめ：");
-            str2  = br.readLine();
-            if(Integer.parseInt(str2)<2||Integer.parseInt(str2)>9999){
+            inputNum2 = Integer.parseInt(br.readLine());
+            if(inputNum2<2||inputNum2>9999){
                 System.out.println("2-9999の値を入力してください");
                 continue;
             }
             break;
         }
-        int inputNum1;
-        int inputNum2;
-        //数値の小さいほうをinputNum1、大きいほうをinputNum2とする
-        if(Integer.parseInt(str1)<=Integer.parseInt(str2)){
-            inputNum1 = Integer.parseInt(str1);
-            inputNum2 = Integer.parseInt(str2);
-        }
-        else {
-            inputNum1= Integer.parseInt(str2);
-            inputNum2= Integer.parseInt(str1);
-        }
 
-        boolean isfirst = true;
+        //数値の小さいほうをinputNum1、大きいほうをinputNum2とする
+        if(inputNum1>=inputNum2){
+            int temp = inputNum1;
+            inputNum1 = inputNum2;
+            inputNum2 = temp;
+        }
         System.out.print(inputNum1+"以上" + inputNum2 +"以下の素数は、");
-        for(int i = inputNum1; i <= inputNum2; i++){
+        outputSosuuList(inputNum1,inputNum2);
+        System.out.print("です。");
+    }
+
+    //2つの引数の範囲内の素数を出力する
+    //出力例：2,3,5,7,11,13,17,19
+    public static void outputSosuuList(int num1 , int num2) throws IOException {
+        boolean isfirst = true;
+        for(int i = num1; i <= num2; i++){
             boolean isSosuu = true;
             for(int j = 2; j<i ; j++){
                 if(i%j==0){
@@ -107,6 +84,5 @@ public class Sosuu {
                 }
             }
         }
-        System.out.print("です。");
     }
 }
